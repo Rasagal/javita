8e1da40bbe0fa35ca0c8e8acf80e80fdd345e317
Carlos Salcedo <carlossalcedocea@gmail.com>
2025-06-30 11:42:17 +0200

    Organización en las carpetas de ejercicios.

--- src/Reto_Semana_4_Viernes_Ejercicio_ValidadorContrasena/ContrasenaInvalidaException.java
+++
@@ -1,8 +0,0 @@
-package Reto_Semana_4_Viernes_Ejercicio_ValidadorContrasena;
-
-public class ContrasenaInvalidaException extends Exception {
-
-    public ContrasenaInvalidaException(String message) {
-        super(message);
-    }
-}
--- src/Reto_Semana_4_Viernes_Ejercicio_ValidadorRegistro/ValidadorRegistro.java
+++
@@ -1,5 +0,0 @@
-package Reto_Semana_4_Viernes_Ejercicio_ValidadorRegistro;
-
-public class ValidadorRegistro {
-
-}
